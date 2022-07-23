edf375133ab6f0a4b273834dc915871603b65f7f
Shoichiro KANNO <s-kanno@qb3.so-net.ne.jp>
2022-07-23 11:33:51 +0900

    初期のコミット

---
+++ index.js
@@ -0,0 +1,13 @@
+VANTA.GLOBE({
+    el: "#background",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    color: 0x3fafff,
+    color2: 0x696f75,
+    backgroundColor: 0x0
+});
